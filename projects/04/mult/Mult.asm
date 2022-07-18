1efb70e6261d5bce913ce18025ade2188516c2e2
Aaron Bos <aaronmbos@gmail.com>
2022-07-18 06:23:43 -0500

    Continue working on project 4

--- projects/04/mult/Mult.asm
+++ projects/04/mult/Mult.asm
@@ -10,23 +10,59 @@
 // R0 >= 0, R1 >= 0, and R0*R1 < 32768.
 
 // Assign variables
-// Need variable to store RAM[0], 1, 2 respectively
+// Need variable to store RAM[0], 1 respectively, sum
 
   @R1
   D=M
+  @x
+  M=D  // x = R1
 
   @R0
   D=M
+  @y
+  M=D  // y = R0
 
+  @sum
+  M=0 
+
+(LOOP)
+  // if (i==x) goto END
+  @i
+  D=M
+  @x
+  D=D-M
+  @STOP
+  D;JEQ
+
+  // sum = sum + y
+  @sum
+  D=M
+  @y
+  D=D+M
+  @sum
+  M=D // sum = sum + y
+
+  @i
+  M=M+1
+
+  @LOOP
+  0;JMP
+
+(STOP)
+  @sum
+  D=M
   @R2
-  D=MI
+  M=D // RAM[2] = sum
 
+(END)
+  @END
+  0;JMP
 // Begin loop
 // RAM[0] will be number added to itself
 // RAM[1] will be loop iteration
 // RAM[2] will be the product
 
-// while n <= RAM[1]
+// for (i=0; i<n; i++)
 // {
 //   RAM[2] = RAM[2] + [RAM0]
 // }
