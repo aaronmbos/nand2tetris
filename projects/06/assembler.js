b9bc3e9e967448bd49d4844d4d317630aa8b5b11
Aaron Bos <aaronmbos@gmail.com>
2022-08-09 06:14:43 -0500

    Continue working on parser

--- projects/06/assembler.js
+++ projects/06/assembler.js
@@ -4,20 +4,66 @@ const pathToFile = process.argv[2];
 const pathSegments = pathToFile.split("/");
 const fileName = pathSegments[pathSegments.length - 1].split(".")[0];
 
-fs.readFile(pathToFile, "utf8", (err, data) => {
-  if (err) {
-    console.error(err);
-    return;
-  }
+const lCommand = "L_COMMAND";
+const aCommand = "A_COMMAND";
+const cCommand = "C_COMMAND";
 
-  const asmLines = data.split("\r\n");
+parse(pathToFile);
 
-  let hackData = "";
-  asmLines.forEach((l) => (hackData += `${l}\n`));
-
-  fs.writeFile(`./${fileName}.hack`, hackData, (err) => {
+function parse(filePath) {
+  fs.readFile(filePath, "utf8", (err, data) => {
     if (err) {
       console.error(err);
+      return;
     }
+
+    const asmLines = data.split("\r\n");
+
+    let hackData = [];
+    asmLines.forEach((l) => {
+      if (isCommand(l)) {
+        let parsed = {};
+        parsed.commandType = parseCommandType(l);
+
+        if (
+          parsed.commandType === aCommand ||
+          parsed.commandType === lCommand
+        ) {
+          parsed.symbol = parseSymbol(l);
+        }
+
+        hackData.push(parsed);
+      }
+    });
+
+    fs.writeFile(
+      `./${fileName}.hack`,
+      hackData.map((x) => x.symbol).join("\n"),
+      (err) => {
+        if (err) {
+          console.error(err);
+        }
+      }
+    );
   });
-});
+}
+
+function isCommand(inputLine) {
+  // Ignore empty lines and comments
+  return inputLine && !inputLine.startsWith("//");
+}
+
+function parseCommandType(inputLine) {
+  if (inputLine.startsWith("(")) {
+    return lCommand;
+  } else if (inputLine.startsWith("@")) {
+    return aCommand;
+  } else {
+    return cCommand;
+  }
+}
+
+function parseSymbol(inputLine) {
+  const regex = /.*[()@]/gm;
+  return inputLine.replace(regex, "");
+}
