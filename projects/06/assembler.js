e0524f9251b0619c84159bd8b6f064288865eee8
Aaron Bos <aaronmbos@gmail.com>
2022-08-11 06:19:12 -0500

    Finish code module and begin final output conversion

--- projects/06/assembler.js
+++ projects/06/assembler.js
@@ -7,6 +7,9 @@ const fileName = pathSegments[pathSegments.length - 1].split(".")[0];
 const lCommand = "L_COMMAND";
 const aCommand = "A_COMMAND";
 const cCommand = "C_COMMAND";
+const destMap = createDestMap();
+const compMap = createCompMap();
+const jumpMap = createJumpMap();
 
 parse(pathToFile);
 
@@ -41,6 +44,11 @@ function parse(filePath) {
             parsed.comp = parts[0];
             parsed.jump = parts[1];
           }
+
+          parsed.destCode = destMap.get(parsed.dest) ?? "000";
+          parsed.jumpCode = jumpMap.get(parsed.jump) ?? "000";
+          parsed.compCode = compMap.get(parsed.comp);
+          parsed.aCode = parsed.comp.includes("M") ? 1 : 0;
         }
 
         hackData.push(parsed);
@@ -50,7 +58,11 @@ function parse(filePath) {
     fs.writeFile(
       `./${fileName}.hack`,
       hackData
-        .map((x) => (x.symbol ? x.symbol : `${x.dest}/${x.comp}/${x.jump}`))
+        .map((x) =>
+          x.symbol
+            ? createBinaryString(Number(x.symbol))
+            : `${x.destCode}/${x.compCode}/${x.jumpCode}`
+        )
         .join("\n"),
       (err) => {
         if (err) {
@@ -59,6 +71,18 @@ function parse(filePath) {
       }
     );
   });
+}
+
+// https://stackoverflow.com/a/24153275/8548471
+// Need to slice first 16 chars because Hack is 16-bit
+function createBinaryString(nMask) {
+  // nMask must be between -2147483648 and 2147483647
+  for (
+    var nFlag = 0, nShifted = nMask, sMask = "";
+    nFlag < 32;
+    nFlag++, sMask += String(nShifted >>> 31), nShifted <<= 1
+  );
+  return sMask.slice(16);
 }
 
 function isCommand(inputLine) {
@@ -108,13 +132,48 @@ function createDestMap() {
 function createJumpMap() {
   const jumpMap = new Map();
 
-    jumpMap.set("JGT", "001");
-    jumpMap.set("JEQ", "010");
-    jumpMap.set("JGE", "011");
-    jumpMap.set("JLT", "100");
-    jumpMap.set("JNE", "101");
-    jumpMap.set("JLE", "110");
-    jumpMap.set("JMP", "111");
+  jumpMap.set("JGT", "001");
+  jumpMap.set("JEQ", "010");
+  jumpMap.set("JGE", "011");
+  jumpMap.set("JLT", "100");
+  jumpMap.set("JNE", "101");
+  jumpMap.set("JLE", "110");
+  jumpMap.set("JMP", "111");
 
   return jumpMap;
+}
+
+function createCompMap() {
+  const compMap = new Map();
+
+  compMap.set("0", "101010");
+  compMap.set("1", "111111");
+  compMap.set("-1", "111010");
+  compMap.set("D", "001100");
+  compMap.set("A", "110000");
+  compMap.set("!D", "001101");
+  compMap.set("!A", "110001");
+  compMap.set("-D", "001111");
+  compMap.set("-A", "110011");
+  compMap.set("D+1", "011111");
+  compMap.set("A+1", "110111");
+  compMap.set("D-1", "001110");
+  compMap.set("A-1", "110010");
+  compMap.set("D+A", "000010");
+  compMap.set("D-A", "010011");
+  compMap.set("A-D", "000111");
+  compMap.set("D&A", "000000");
+  compMap.set("D|A", "010101");
+  compMap.set("M", "110000");
+  compMap.set("!M", "110001");
+  compMap.set("-M", "110011");
+  compMap.set("M+1", "110111");
+  compMap.set("M-1", "110010");
+  compMap.set("D+M", "000010");
+  compMap.set("D-M", "010011");
+  compMap.set("M-D", "000111");
+  compMap.set("D&M", "000000");
+  compMap.set("D|M", "010101");
+
+  return compMap;
 }
